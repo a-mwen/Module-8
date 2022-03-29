a5ce6de06a2bd48eb326c2f171a084160fad64c5
a-mwen <82180635+a-mwen@users.noreply.github.com>
2022-03-28 21:33:01 -0400

    Add files via upload

---
+++ sandbox.js
@@ -0,0 +1,47 @@
+//This pulls the data based on the paragraph tag
+//const para = document.querySelector('p;');
+
+//This pulls the error based on the div tag
+//const para = document.querySelector('div.error');
+
+//const para = document.querySelectorAll('p;');
+//console.log(para);
+
+//This is a loop to access all of the data on the screen
+
+/*
+paras.forEach(para => {
+    console.log(para);
+}};
+*/
+
+//Performing a GetEelementById
+const title = document.getElementById('page-title');
+const errors = document.getElementsByClassName('error');
+//console.log(errors[0]);
+
+//For each in this methodology showuld be prohibited
+/*
+errors.array.forEach(error) => {
+    console.log(error);    
+});
+*/
+
+// This uses a method to use InnerText and append an existing tag base
+/*
+const para = document.querySelector('p');
+para.innerText += ' Today is an awesome day';
+console.log(para.innerText);
+*/
+
+//const para = document.querySelector('p');
+/*
+const paras = document.querySelectorAll('p');
+
+paras.forEach(para => {
+    console.log(para.innerText);
+})
+
+
+
+*/
